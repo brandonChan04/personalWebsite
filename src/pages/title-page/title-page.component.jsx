0c0604154d0aa6bd9367141df43293348250b0bb
jgrey2550 <jgrey2550@gmail.com>
2025-04-21 22:35:43 -0700

    styling changes

--- src/pages/title-page/title-page.component.jsx
+++ src/pages/title-page/title-page.component.jsx
@@ -9,15 +9,24 @@ function TitlePage() {
             <div>
                 <h1>Hi I'm Brandon<br/>An Aspiring Developer</h1>
                 <div className="nav-links">
-                    <a href="https://www.linkedin.com/in/brandon-chan-a10831165/" target="_blank" rel="noopener noreferrer">
-                        <Linkedin className="icon" />
-                    </a>
-                    <a href="https://github.com/brandonChan04" target="_blank" rel="noopener noreferrer">
-                        <Github className="icon" />
-                    </a>
-                    <a href={resume} download="BrandonChanResume.pdf" target="_blank" rel="noopener noreferrer">
-                        <Newspaper className="icon" />
-                    </a>
+                    <div className="icon-wrapper">
+                        <a href="https://www.linkedin.com/in/brandon-chan-a10831165/" target="_blank" rel="noopener noreferrer">
+                            <Linkedin className="icon" />
+                        </a>
+                        <span className="tooltip">LinkedIn</span>
+                    </div>
+                    <div className="icon-wrapper">
+                        <a href="https://github.com/brandonChan04" target="_blank" rel="noopener noreferrer">
+                            <Github className="icon" />
+                        </a>
+                        <span className="tooltip">GitHub</span>
+                    </div>
+                    <div className="icon-wrapper">
+                        <a href={resume} download="BrandonChanResume.pdf" target="_blank" rel="noopener noreferrer">
+                            <Newspaper className="icon" />
+                        </a>
+                        <span className="tooltip">Resume</span>
+                    </div>
                 </div>
             </div>
             <div>
