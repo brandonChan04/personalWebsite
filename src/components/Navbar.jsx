d1fc2e4d52b0164743e6ba0c39d3b92d00ea9c3d
jgrey2550 <jgrey2550@gmail.com>
2025-04-28 21:43:48 -0700

    added favicon and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import "../styling/navbar.css";
+import logo from "../images/logo.png";
 
 const scrollToSection = (id) => {
   const section = document.getElementById(id);
@@ -8,7 +9,7 @@ const scrollToSection = (id) => {
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo" onClick={() => scrollToSection("title")}>Brandon</h1>
+      <img src={logo} className="logo" alt="Logo" onClick={() => scrollToSection("title")}/>
 
       <div className="sections">
         <span onClick={() => scrollToSection("projects")}>Projects</span>
