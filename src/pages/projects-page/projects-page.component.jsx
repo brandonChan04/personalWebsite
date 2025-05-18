09f78e9847ff7cf1a95fd10f004c3f6d0fa8ea0f
jgrey2550 <jgrey2550@gmail.com>
2025-05-18 00:23:40 -0700

    added paytrack + updated resume

--- src/pages/projects-page/projects-page.component.jsx
+++ src/pages/projects-page/projects-page.component.jsx
@@ -5,13 +5,26 @@ import factoryEscape from "../../images/factory_escape.png";
 import website from "../../images/p_website.png";
 import recordKeeper from "../../images/record_keeper.png";
 import typle from "../../images/typle.png";
+import paytrack from "../../images/paytrack.png";
 import "./projects-page.component.css"
 
 const ProjectsPage = () => {
   const projectList = [
     {
+      name: "Paytrack",
+      description: "Paytrack is a simple but powerful web application that allows users to track their outgoing invoices. Using VueJS and Firebase, Paytrack allows users to create, edit, and delete invoices sending them to their clients and allowing for easy tracking of payments.",
+      image: paytrack,
+      github: "https://github.com/brandonChan04/paytrack",
+      website: "https://paytrack-sable.vercel.app/",
+      tags: [
+        'VueJS',
+        'Vite',
+        'Firebase',
+      ]
+    },
+    {
       name: "Typle",
-      description: "Typle is an online multiplayer web applicaiton. Using the MERN stack it allows users to track progression through levels and challenges. Typle also allows for real time online multiplayer battles using Socket.io",
+      description: "Typle is an multithreaded online multiplayer web applicaiton. Using the MERN stack it allows users to track progression through levels and challenges. Typle also allows for real time online multiplayer battles using Socket.io",
       image: typle,
       github: "https://github.com/brandonChan04/typleFrontend",
       website: "https://typle-frontend-v2.vercel.app/",
